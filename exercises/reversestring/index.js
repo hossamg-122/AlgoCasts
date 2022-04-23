77907686f0633a674c8c7dcce06442e28827eb1d
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-04-24 01:06:25 +0200

    feat : reverse string algorithm

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split("").reduce((reverced, char) => char + reverced, "");
+}
 
 module.exports = reverse;
+
+//1st solution
+// return str.split("").reverse().join("")
+
+// 2n solution
+//let reversedString = ""
+// for (let i = 0; i < str.length; i++) {
+//     const element = str[i];
+//     reversedString = element+reversedString
+// }
+// return reversedString
+
+// or
+
+//let reversedString = ""
+// for (const char of str) {
+//     reversedString=char+reversedString
+// }
+// return reversedString
+
+//2nd solution
+// let reversedString = []
+// for (let i = 0; i < str.length; i++) {
+//     const element = str[i];
+//     reversedString.unshift(element)
+// }
+// return reversedString.join("")
+
+// 4th solution
+// return str.split("").reduce((reverced,char)=>char+reverced,"")
