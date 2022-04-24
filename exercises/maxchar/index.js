a4f102c091ba9434bfd46de98874b6b163f47175
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-04-24 05:14:26 +0200

    feature : maxChar algorithm

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,14 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charsContainer = {};
+  for (const char of str) {
+    charsContainer[char] ? ++charsContainer[char] : charsContainer[char] = 1
+    
+  }
+ 
+ return Object.keys(charsContainer).reduce((a,b)=> charsContainer[a] > charsContainer[b]? a :b)
+}
 
 module.exports = maxChar;
