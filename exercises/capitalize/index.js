8fb49030ee3357b28702ee010b0e414417bca214
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-04-25 02:37:57 +0200

    feature : capitalize algorithm

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,45 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let capitalizedString = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    str[i - 1] === " "
+      ? (capitalizedString = capitalizedString + str[i].toUpperCase())
+      : (capitalizedString = capitalizedString + str[i]);
+  }
+  return capitalizedString;
+}
 
 module.exports = capitalize;
+
+// 1st solution
+// function capitalize(str) {
+//     return str
+//       .split(" ")
+//       .map((word) => {
+//         return word[0].toUpperCase() + word.slice(1);
+//       })
+//       .join(" ");
+//   }
+
+// 2nd solution
+// function capitalize(str) {
+//     const modifiedString = str.split(" ");
+//     let capitalizedString = []
+//     for (const word of modifiedString) {
+//         capitalizedString.push(word[0].toUpperCase() + word.slice(1))
+//     }
+//     return capitalizedString.join(" ")
+//   }
+
+// 3rd solution
+// function capitalize(str) {
+//     let capitalizedString = str[0].toUpperCase();
+//     for (let i = 1; i < str.length; i++) {
+//       str[i - 1] === " "
+//         ? (capitalizedString = capitalizedString + str[i].toUpperCase())
+//         : (capitalizedString = capitalizedString + str[i]);
+//     }
+//     return capitalizedString;
+//   }
