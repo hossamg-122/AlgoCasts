e449ec3b533a960d5e947515cec1d0925f029b8d
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-05-01 05:50:49 +0200

    feature : pyramid algorithm

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,54 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n,row=0,str="") {
+    if (n === row){
+        return
+    }
+    let cols = (n * 2) - 1
+    if(str.length===cols){
+        console.log(str)
+     return pyramid(n,row+1)
+    }
+    let leftRange = n-1-row
+        let rightRange = n-1+row
+        str.length>=leftRange && str.length<=rightRange ? str+="#":str+=" "
+        pyramid(n,row,str)
+}
 
 module.exports = pyramid;
+
+// 1st solution
+// function pyramid(n) {
+//     if (n < 0){
+//         return
+//     }
+//     let cols = (n * 2) - 1
+//     for (let i = 0; i < n; i++) {
+//         let row = []
+//         let leftRange = n-1-i
+//         let rightRange = n-1+i
+//         for (let j = 0; j < cols; j++) {
+          
+//           j>=leftRange && j<=rightRange ? row.push("#"):row.push(" ")
+            
+//         }
+//         console.log(row.join(""))
+//     }
+// }
+
+// 2nd recursive solution
+// function pyramid(n,row=0,str="") {
+//     if (n === row){
+//         return
+//     }
+//     let cols = (n * 2) - 1
+//     if(str.length===cols){
+//         console.log(str)
+//      return pyramid(n,row+1)
+//     }
+//     let leftRange = n-1-row
+//         let rightRange = n-1+row
+//         str.length>=leftRange && str.length<=rightRange ? str+="#":str+=" "
+//         pyramid(n,row,str)
+// }
