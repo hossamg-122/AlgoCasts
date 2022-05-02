c02f1e1f69934651166228dd07ffefcf04bfc761
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-05-02 23:12:54 +0200

    feature : vowels algorithm

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,45 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let modifiedString = str.replace(/[^\w]/g, "").toLowerCase()
+    let counter =0
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+
+    for (const char of modifiedString) {
+      vowels.includes(char) && ++counter
+    }
+  
+return counter
+  
+}
 
 module.exports = vowels;
+
+// 1st solution
+// function vowels(str) {
+//     let modifiedString = str.replace(/[^\w]/g, "").toLowerCase()
+//     let stringContainer = {}
+//     let vowels = ['a', 'e', 'i', 'o', 'u']
+//     for (const char of modifiedString) {
+//         stringContainer[char] ? ++stringContainer[char] : stringContainer[char] =1
+//     }
+//    return Object.keys(stringContainer).reduce((counter,char)=>{
+// if(vowels.includes(char) ){
+// ++counter
+// }
+// return counter
+//     },0)
+// }
+
+
+// 2nd solution
+// function vowels(str) {
+//     let modifiedString = str.replace(/[^\w]/g, "").toLowerCase()
+//     let counter =0
+//     let vowels = ['a', 'e', 'i', 'o', 'u']
+//     for (const char of modifiedString) {
+//       vowels.includes(char) && ++counter
+//     }
+// return counter
+// }
